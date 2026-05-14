2199f3983b7ae381908e9cb6082b3881494d5c6a
Md Javed <mdjaved17@users.noreply.github.com>
2026-05-14 14:18:39 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import connectDb from './config/connectDb.js';
+
+dotenv.config();
+
+const app = express();
+
+
+const PORT = process.env.PORT || 6000;
+
+
+app.get('/', (req, res) => { 
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDb();
+});
